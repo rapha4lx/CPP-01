fecb3ebaf84bdfb1771d2344e3dad83e012ac904
rapha4lx <rapha4.lx@gmail.com>
2025-10-17 14:59:00 -0300

    started ex05

---
+++ ex05/Harl.hpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+class Harl
+{
+private:
+private:
+    void complain(std::string level);
+public:
+    Harl();
+    ~Harl();
+
+    void debug();
+    void info();
+    void warning();
+    void error();
+};
+
+Harl::Harl()
+{
+}
+
+Harl::~Harl()
+{
+}
+
+void Harl::debug()
+{
+
+}
+
+void Harl::info()
+{
+
+}
+
+void Harl::warning()
+{
+
+}
+
+void Harl::error()
+{
+    
+}
+
